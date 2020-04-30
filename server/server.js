eb6d5c8ee7d9969cea60f99404deb362d819b434
Chris Aragon <aragonchristopherray@gmail.com>
2020-04-30 11:58:46 -0400

    number is undefined

--- server/server.js
+++ server/server.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const MOVIES = require('./movie-data.json')
+const cors = require('cors')
+const helmet = require('helmet')
 
 
 const app = express();
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
 
 console.log(process.env.API_TOKEN)
 
@@ -23,20 +27,28 @@ function handleMovies(req,res){
     debugger
     if(req.query.genre){
         response = response.filter(movie => {
-            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+            return(
+                movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+            )
+            
         })
     }
     if(req.query.country){
         response = response.filter(movie => {
-            movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+            return(
+                movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+            )  
         })
     }
     if(req.query.avg_vote){
         response = response.filter(movie => {
-            movie.avg_vote.includes(req.query.avg_vote)
+            let number = Number(req.query.avg_vote)
+            return(
+              movie.ave_vote.includes(number)
+            ) 
         })
     }
-    res.json(response)
+    res.send(response)
 }
 app.get('/movies', handleMovies)
 
