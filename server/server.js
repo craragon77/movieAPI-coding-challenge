a1d71829f0411b4fd650a97249911fd6668fe5f9
Chris Aragon <aragonchristopherray@gmail.com>
2020-05-01 14:31:27 -0400

    pushing to Heroku

--- server/server.js
+++ server/server.js
@@ -7,11 +7,10 @@ const helmet = require('helmet')
 
 
 const app = express();
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet());
 app.use(cors());
-
-console.log(process.env.API_TOKEN)
 
 app.use(function validateBearerToken(req,res,next){
     console.log('validate bearer token middleware')
@@ -51,7 +50,17 @@ function handleMovies(req,res){
 }
 app.get('/movies', handleMovies)
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production'){
+        response = {error: {message: 'server error'}}
+    } else {
+        response = {error}
+    }
+    res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 app.listen(PORT, () =>{
     console.log(`Server listening at http://localhost:${PORT}`)
 })
